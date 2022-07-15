9418c38d158de6332722b37d06fe4084e1c7fa67
laskovic <bartek.lachacz125@gmail.com>
2022-07-15 13:50:33 +0200

    adding class toggling

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 'use strict'
 
+const hamburgerButton = document.querySelector(".burger-open");
+const hamburgerMenuSection = document.querySelector(".wrapper2")
+
+hamburgerButton.addEventListener('click', function(){
+    hamburgerMenuSection.classList.toggle("wrapper2")
+})
