5519573e20651e26ee3f24b8a4829816a00f3683
laskovic <bartek.lachacz125@gmail.com>
2022-07-18 10:11:52 +0200

    finishing touches, focusing on adding tailwind to the project

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 
 const hamburgerButton = document.querySelector(".burger-open");
 const hamburgerMenuSection = document.querySelector(".wrapper2")
+const navBar = document.querySelector('.wrapper')
 
 hamburgerButton.addEventListener('click', function(){
     hamburgerMenuSection.classList.toggle("wrapper2")
+    hamburgerButton.classList.toggle("toggled3")
 })
